21bb5d5d35073d83750eb08db6df1a500e020851
huge-github <67527272+huge-github@users.noreply.github.com>
2021-12-04 17:11:14 +0800

    pull commit

--- src/main/java/com/huge/git/GitTest.java
+++ src/main/java/com/huge/git/GitTest.java
@@ -9,5 +9,6 @@ public class GitTest {
         System.out.println("mater git345");
         System.out.println("hot-fix git234");
         System.out.println("push commit");
+        System.out.println("pull commit");
     }
 }
